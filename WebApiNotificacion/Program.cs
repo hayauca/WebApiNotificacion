0e5e17e24c1eb3df79f4654739f6e5dc323d10e9
hayauca <hayauca@outlook.es>
2024-09-04 12:50:38 -0500

    Primer commit WebApiNotificacion

---
+++ WebApiNotificacion/Program.cs
@@ -0,0 +1,52 @@
+using Application.Exceptions;
+using Application.Mappings;
+using Application.Services;
+using Core.Interfaces;
+using Infrastructure.Repositories;
+using WebApiNotificacion;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+string oracleConnectionString = builder.Configuration.GetConnectionString("OracleDb"); //hac
+
+// Add services to the container.
+builder.Services.AddScoped<ICustomerRepository>(provider => new CustomerRepository(oracleConnectionString)); //hac
+builder.Services.AddScoped<CustomerService>(); //hac
+
+
+// Agregar AutoMapper al contenedor de servicios
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly); //hac
+
+
+builder.Services.AddControllers();
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configura el middleware y el pipeline de solicitudes
+app.UseMiddleware<ExceptionHandlingMiddleware>(); //hac
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
